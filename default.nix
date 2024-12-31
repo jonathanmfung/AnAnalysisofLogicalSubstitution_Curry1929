351e95845a55f8af5e2eac3aac35a6fc3a3739b2
Jonathan Fung <jonathanfung2000@gmail.com>
2024-12-31 15:02:40 -0800

    init

---
+++ default.nix
@@ -0,0 +1,56 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  pkgs = import nixpkgs {
+    config = { };
+    overlays = [ ];
+  };
+  stdenv = pkgs.stdenv;
+  tex = pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+      scheme-basic
+      latex-bin
+      luatex
+      latexmk
+      footmisc
+      todonotes
+      titlesec
+      enumitem
+      # etoolbox
+      # fmtcount
+      # xkeyval
+      # ragged2e
+      # fontspec
+      ;
+  };
+in
+{
+  document = stdenv.mkDerivation rec {
+    name = "cover_letter";
+    src = ./.;
+    nativeBuildInputs = [
+      pkgs.coreutils
+      pkgs.findutils
+      tex
+    ];
+
+    TZDIR = "${pkgs.tzdata}/share/zoneinfo";
+    FONTCONFIG_FILE = pkgs.makeFontsConf { fontDirectories = [ pkgs.source-sans-pro ]; };
+
+    buildPhase = ''
+      runHook preBuild
+      export PATH="${pkgs.lib.makeBinPath nativeBuildInputs}"
+      export TZ="America/Los_Angeles"
+      mkdir -p .cache/texmf-var
+      env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+        latexmk -interaction=nonstopmode -pdf -lualatex \
+        document.tex
+      runHook postBuild
+    '';
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out
+      cp document.pdf $out/
+      runHook postInstall
+    '';
+  };
+}
